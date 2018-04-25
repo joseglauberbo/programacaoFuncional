641da3d386d1e9678bbcf4fef2e36f36ff38c004
Glauber <jose.oliveira@ccc.ufcg.edu.br>
2018-04-25 14:30:40 -0300

    implementando lista dois

--- funcoes-sintaxe.hs
+++ funcoes-sintaxe.hs
@@ -2,6 +2,7 @@
 - Dada uma tupla, divide o primeiro pelo segundo usando pattern matching.
 - Ela deve ser indefinida quando o denominador for zero.
 -}
+
 divTuple (x, y) = undefined
 
 {-
@@ -9,7 +10,8 @@ divTuple (x, y) = undefined
  - Ex: somatorio 0 1 = 1
  -     somatorio 1 3 = 6
 -}
-somatorio a b = undefined 
+
+somatorio a b = undefined
 
 {-
  - Calcula o somatorio (recursivo) entre dois numeros a e b (a < b).
@@ -19,10 +21,15 @@ somatorio a b = undefined
 somatorioRec a b = undefined
 
 -- Defina a funcao que eleva um membro ao quadrado
-square x = undefined 
+square 0 = 0
+square 1 = 1
+square x = x * x 
 
 -- Soma os quadrados de dois numeros.
-sumSquares x y = undefined
+
+sumSquares 0 0 = 0
+sumSquares 1 1 = 1
+sumSquares x y = square x + square y
 
 -- Defina uma funcao de alta ordem que aceita uma função (Int -> Int) e aplica a funcao a dois numeros
 higherOrderSum f a b = undefined
@@ -33,3 +40,11 @@ hoSumSquares = undefined
 --Implemente a funcao mapFilter que primeiro aplica o map de uma funcao f a uma lista e depois aplica a funcao filter
 -- a lista resultante. Procure usar a composicao de funcoes
 mapFilter f p xs = undefined
+
+
+main:: IO()
+main = do
+	x <- getLine
+	y <- getLine
+	let resposta = sumSquares (read x) (read y)
+	print resposta
