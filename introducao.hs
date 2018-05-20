0f023b612352cab59cc39196e50e096d91d7ddeb
José Glauber <jose.oliveira@ccc.ufcg.edu.br>
2018-05-20 18:47:12 -0300

    fazendo mais um pouco da lista introdutória

--- introducao.hs
+++ introducao.hs
@@ -1,45 +1,3 @@
-{-
-- Usando os predicados not,and e or prontos de Haskell, implemente os predicados (funcoes) xor (or exclusivo),
-- impl (implicacao A => B é equivalente a (not A or B)) e equiv (A <=> B é definido como A => B and B => A)
-- Procure usar casamento de padroes e reutilizar as funcoes.
--}
-
-
-xor:: Bool -> Bool -> Bool 
-xor a b | a && not b = True
-		| not a && b = True
-		| otherwise = False
-
-impl:: Bool -> Bool -> Bool
-impl a b = (not a) || b
-
-equiv:: Bool -> Bool -> Bool
-equiv a b = (impl a b) && (impl b a)
-
-{-
-A funcao square esta implementada e eleva ao quadrado um determinado numero
--}
-
-square:: Int -> Int
-square x = x*x
-
-{-
-- Implemente a funcao potencia, que retorna o resultado de x elevado a y 
--}
-
-pow:: Int -> Int -> Int
-pow x 0 = 1
-pow x 1 = x
-pow x y = x * pow x (y-1) 
-
-{-
-- Implemente a funcao fatorial que calcula o fatorial de um numero 
--}
-
-fatorial:: Int -> Int
-fatorial 0 = 1
-fatorial 1 = 1
-fatorial x = x * fatorial(x-1)
 
 {-
 - Determina se um numero eh primo ou nao. Preocupe-se apenas em resolver o problema.
@@ -47,42 +5,8 @@ fatorial x = x * fatorial(x-1)
 -}
 
 isPrime:: Int -> Bool
-isPrime x = [g | g <- [1..x], x`mod`g == 0]
+isPrime x = 
+	let lista = [g | g <- [1..x], x `mod` g == 0] 
+	in 
+		if length lista == 2 then True else False
 
-isPrime2:: [Int] -> Int -> Bool
-isPrime2 (x:xs) x | x == 1 && 
-
-
-
-{-
-- Calcula um termo da sequencia de Fibonnacci. Voce pode trabalhar com listas. 
--}
-fib x = undefined
-
-{-
-- Calcula um MDC de dois numeros usando o algoritmo de Euclides. 
--}
-mdc x y = undefined
-
-{-
-- Calcula um MMC de dois numeros. 
--}
-mmc x y = undefined
-
-{-
-- Determina se dois numeros inteiros positivos sao co-primos. Dois numeros sao co-primos se 
-- o mdc deles for igual a 1. Ex: coprimo 35 64 = True 
--}
-coprimo x y = undefined
-
-{-
-- Calcula a conjectura de Goldbach, que diz que um numero par maior que 2 pode ser escrito como a soma de dois numeros primos. Ex: 28 = 5 + 23.
--}
-goldbach x = undefined
-
-main:: IO()
-main = do
-	x <- getLine
-	y <- getLine
-	let resposta = equiv (read x) (read y)
-	print resposta
--- tipos-de-dados.hs
+++ tipos-de-dados.hs
@@ -1,42 +1,71 @@
 --Escreva a declaracao para o tipo Triple, contendo tres elementos, todos de tipos diferentes.
 --Escreva funcoes tripleFst, tripleSnd, tripleThr para extrair respectivamente o primeiro, segundo e terceiro
 -- elementos de uma triple.
-data Triple a b c = Nada deriving (Eq,Show)
+data Triple a b c = Triple a b c deriving (Eq,Show)
 
-tripleFst = undefined
-tripleSnd = undefined
-tripleThr = undefined
+tripleFst:: (Triple a b c) -> a
+tripleFst (Triple a b c) = a
+
+tripleSnd:: (Triple a b c) -> b
+tripleSnd (Triple a b c) = b
+
+tripleThr:: (Triple a b c) -> c
+tripleThr (Triple a b c) = c
 
 --Escreva um tipo Quadruple que contem 4 elementos: dois de um mesmo tipo e outros dois de outro tipo
 --Escreva as funcoes frstTwo e secondTwo que retornam os dois primeiros e os dois ultimos, respectivamente
-data Quadruple a b = Vazio
+data Quadruple a b = Quadruple a a b b deriving (Eq, Show)
 
-firstTwo = undefined
-secondTwo = undefined
+firstTwo:: (Quadruple a b) -> [a]
+firstTwo (Quadruple x y w z)= [x,y]
+
+secondTwo:: (Quadruple a b) -> [b]
+secondTwo (Quadruple x y w z)= [w,z]
 
 --Escreva um tipo de dados que pode conter um, dois, tres ou quatro elementos, dependendo do construtor
 --Implemente funções tuple1 até tuple4 que que retornam Just <valor> ou Nothing se o valor nao existe
-data Tuple a b c d = NVazio
+data Tuple a b c d = Tuple1 a | Tuple2 a b | Tuple3 a b c | Tuple4 a b c d deriving (Eq, Show)
 
-tuple1 = undefined 
-tuple2 = undefined 
-tuple3 = undefined 
-tuple4 = undefined 
+tuple1:: (Tuple a b c d) -> Maybe a
+tuple1 (Tuple1 x) = Just x
+tuple1 (Tuple2 x w) = Just x
+tuple1 (Tuple3 x w y) = Just x
+tuple1 (Tuple4 x w y z) = Just x
 
+tuple2:: (Tuple a b c d) -> Maybe b
+tuple2 (Tuple2 x w) = Just w
+tuple2 (Tuple3 x w y) = Just w
+tuple2 (Tuple4 x w y z) = Just w
+tuple2 _ = Nothing
+
+tuple3:: (Tuple a b c d) -> Maybe c
+tuple3 (Tuple3 x w y) = Just y
+tuple3 (Tuple4 x w y z) = Just y
+tuple3 _ = Nothing
+
+tuple4:: (Tuple a b c d) -> Maybe d
+tuple4 (Tuple4 x w y z) = Just z
+tuple4 _ = Nothing
+
+--Escreva funções que retornem o tamanho, a cabeça, o tail.
+--Faça o somatorio a esquerda de um valor inicial, uma função e uma lista.
+--Faça o somatorio a direita de um valor inicial, uma função e uma lista.
 data List a = Nil | Cons a (List a) deriving (Eq,Show)
 
+listLength:: (List a) -> Integer
 listLength Nil = 0
 listLength (Cons x xs) = 1 + listLength xs
 
+listHead:: (List a) -> a
 listHead Nil = error "Empty list"
 listHead (Cons x xs) = x
 
+listTail:: (List a) -> (List a)
 listTail Nil = Nil
 listTail (Cons x xs) = xs
 
 listFoldr f v Nil = v
 listFoldr f v (Cons x xs) = f x (listFoldr f v xs)
-
 
 listFoldl f v Nil = v
 listFoldl f v (Cons x xs) = listFoldl f (f v x) xs 
